2dcc8dcd80d39bad936c1c6f4491abddf46f0eb3
fazfaz7 <adrian.faz123@gmail.com>
2024-06-15 16:40:47 -0600

    Initial Commit

---
+++ RocketHack/RocketHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketHackApp.swift
+//  RocketHack
+//
+//  Created by Adrian Faz on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RocketHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
